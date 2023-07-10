a128320e9b1313cb7f9ea7d1b096a13789b144bd
MagcDev <manuel117@gmail.com>
2023-07-10 14:25:32 -0400

    To eassy

---
+++ README.md
@@ -0,0 +1,10 @@
+### Pruba tecnica para Trainess y Juniors
+
+
+APIs: 
+
+Facts: https://catfact.ninja/fact
+Images: https://cataas.com/cat/says/${firstword}
+
+
+Recuper un hecho aleatorio de gatos y muestra una imagen de un gato con la primare palabra del hecho
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,44 @@
+import { useEffect, useState } from "react"
+
+interface MyApiDataFact {
+  fact: string
+  length: number
+}
+
+interface MyApiDataCat {
+  id: string
+  url: string
+}
+
 function App() {
+  const [fact, setFact] = useState("")
+  const [imageUrl, setImageUrl] = useState("")
+
+
+  useEffect(() => {
+
+    async function fetchData() {
+      const factResp = await fetch('https://catfact.ninja/fact')
+      const factData: MyApiDataFact = await factResp.json() as MyApiDataFact;
+
+      setFact(factData.fact);
+
+      const firstwords = factData.fact.split(" ").slice(0, 3).join(" ");
+
+      const response = await fetch(`https://cataas.com/cat/says/${firstwords}`)
+      setImageUrl(response.url)
+    }
+
+    void fetchData()
+  
+  }, [])
+  
 
   return (
-    <>
-      <h1 className="text-4xl font-bold">Hello World</h1>
-    </>
+    <div className="flex flex-col items-center justify-center h-screen">
+      {fact && <h1 className="text-2xl font-bold">{fact}</h1>}
+      {imageUrl && <img className="w-1/3 mt-2" src={imageUrl} alt="cats ramdom fact" />}
+    </div>
   )
 }
 
